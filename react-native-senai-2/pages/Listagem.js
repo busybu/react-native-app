3a7df2ee02df9d026216900df1d6612ce123b0d6
busybu <gabrielle.fialkoski@hotmail.com>
2023-06-27 17:05:42 -0300

    puxando dados para o front end

--- react-native-senai-2/pages/Listagem.js
+++ react-native-senai-2/pages/Listagem.js
@@ -1,15 +1,23 @@
 import { Text, View } from 'react-native'
 import home from '../styles/home';
 import axios from 'axios';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 
 export default function Listagem() {
+	const [transactions, setTransactions] = useState([])
 
-	useEffect(() => { 
-		const getTransactions = async() =>{
-			const response = await axios.get('http://localhost:3000/transactions')
-			console.log(response)	
+	useEffect(() => {
+		const getTransactions = async () => {
+			try {
+				const response = await axios.get('http://localhost:3000/transactions')
+				if (response.status === 200) {
+					setTransactions(response.data)
+				}
+			}
+			catch (err) {
+				console.log(err)
+			}
 		}
 
 		getTransactions()
@@ -18,9 +26,26 @@ export default function Listagem() {
 
 	return (
 
-    <View style={home.buttonText}>
-		<Text>Minhas Transações</Text>
-	</View>
-	
-  );
+		<View style={home.container}>
+			<View style={home.topScreen}>
+				<Text>Minhas Transações</Text>
+
+				<View style={home.button}>
+					<Text>+</Text>
+				</View>
+			</View>
+
+			{ transactions.map((transactions, index) => {
+				return (
+					<View>
+						<Text>{ transactions.reference }</Text>
+						<Text>{ transactions.amount }</Text>
+						<Text>{ transactions.date }</Text>
+					</View>
+				)
+			}) }
+
+		</View>
+
+	);
 }
--- react-native-senai-2/styles/home.js
+++ react-native-senai-2/styles/home.js
@@ -1,32 +1,38 @@
 import { StyleSheet } from 'react-native'
 
 const styles = StyleSheet.create({
-	container: {
-	  flex: 1,
-	  backgroundColor: '#46387C',
-	  alignItems: 'center',
-	  justifyContent: 'center',
-	},
-	button : {
-	  backgroundColor : '#00D8FF',
-	  width : 156,
-	  height : 45,
-	  justifyContent : 'center',
-	  alignItems : 'center',
-	  borderRadius : 10,
-	  marginTop : 15
+    container: {
+        flex: 1,
+        backgroundColor: '#46387C',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    button: {
+        backgroundColor: '#00D8FF',
+        width: 40,
+        height: 40,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 100,
+        marginTop: 0
 
-	},
-	buttonText: { color: '#302850', fontSize: 16 },
-	inputWrapper : {
-		width : 306,
-		height : 45,
-		backgroundColor : '#302850',
-		borderRadius : 10,
-		color : '#0BD8FD',
-		paddingHorizontal : 15,
-		margin : 8
-	}
-  });
+    },
+    topScreen: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        height: 40,
+    },
+    buttonText: { color: '#302850', fontSize: 16 },
+    inputWrapper: {
+        width: 306,
+        height: 45,
+        backgroundColor: '#302850',
+        borderRadius: 10,
+        color: '#0BD8FD',
+        paddingHorizontal: 15,
+        margin: 8
+    }
+});
 
-  export default styles
+export default styles
