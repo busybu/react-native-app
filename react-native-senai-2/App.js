e5fac1f0562f221689e947db02e5d50172890e46
busybu <gabrielle.fialkoski@hotmail.com>
2023-06-27 15:55:35 -0300

    added: api starter

--- react-native-senai-2/App.js
+++ react-native-senai-2/App.js
@@ -1,78 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { useEffect, useState } from 'react';
-import { Button, StyleSheet, Text, View, TextInput, Image, TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
-import Logo from './assets/logo.png'
-import CustomInput from './components/CustomInput';
-import styles from './styles/styles';
-import app from './firebaseConfig';
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from '@firebase/auth';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Login from './pages/Login';
+import Home from './pages/Home';
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
-  const [email, setEmail] = useState()
-  const [senha, setSenha] = useState()
-  const auth = getAuth(app);
- 
-  useEffect(() => {
-    console.log(auth.currentUser)
-    // trocar de tela caso exista um usuario
-  }, [auth.currentUser])
 
-  const setEmailInputValue = (value) => {
-    setEmail(value)
-  }
-
-  const setSenhaInputValue = (value) => {
-    setSenha(value)
-  }
-
-  const criarUsuario = () => {
-    createUserWithEmailAndPassword(auth, email, senha)
-    .then((dadosDoUsuario) => {
-      console.log(dadosDoUsuario)
-    }).catch((err) => {
-      console.error(err)
-    })
-  }
-
-  const loginUsuario = () => {
-    signInWithEmailAndPassword(auth, email, senha)
-    .then((dadosDoUsuario) => {
-      console.log(dadosDoUsuario)
-    }).catch((err) => {
-      console.error(err)
-    })
-  }
- 
   return (
-    <View style={styles.container}>
-      <Image
-        source={Logo}
-        style={{
-          width: 100,
-          height: 100
-        }}
-      />
-
-      <CustomInput
-        placeholder='E-mail'
-        value={email} 
-        funcao={setEmailInputValue}
-      />
-
-      <CustomInput
-        placeholder='Senha'
-        value={senha}
-        funcao={setSenhaInputValue}
-        isPassword={true}
-      />
-
-      <TouchableOpacity style={{ marginTop: 15 }} onPress={loginUsuario}>
-        <View style={styles.button}>
-          <Text style={styles.buttonText}>Entrar</Text>
-        </View>
-      </TouchableOpacity>
-    </View>
-  );
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login}/>
+        <Stack.Screen name="Home" component={Home}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
-
-
---
+++ react-native-senai-2/pages/Home.js
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+import home from '../styles/home';
+
+
+
+export default function Home() {
+
+	return (
+    <View style={home.buttonText}>
+		<Text>oi</Text>
+	</View>
+  );
+}
